de225c8c0c5bacc379cb5edfb8be0f0c93e88615
Navanshu <navanshu.surve1111@gmail.com>
2025-11-02 16:19:15 +0530

    deployment attempt 3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/navanshu-portfolio/",
-
-})
+  base: "/navanshu-portfolio/", // 👈 IMPORTANT: must match your repo name
+});
